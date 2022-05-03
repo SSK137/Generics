7d5af16097952a11586cf211c70f24a4cb66e468
SSK137 <saurabhkodam@gmail.com>
2022-05-03 12:47:18 +0530

    Generics : Maximun From 3 Integer Value

--- src/Generics.java
+++ src/Generics.java
@@ -1,5 +1,17 @@
 public class Generics {
+ public int maxOfValues(int a1,int a2,int a3) {
+        int max = a1;
+        if (a2 > max)
+            max = a2;
+        if (a3 > max)
+            max = a3;
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Generics");
+        Generics generics=new Generics();
+        Generics gns=new Generics();
+        System.out.println("\nWelcome To Find Maximum Value Problem Using Generics \n");
+        System.out.println("The maximum value between the three Number is : " +generics.maxOfValues(8, 9, 5));
     }
 }
