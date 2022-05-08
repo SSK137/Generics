8b4e64b06eea503ed2def3b99d97fc197b266f68
SSK137 <saurabhkodam@gmail.com>
2022-05-08 14:19:07 +0530

    Refactore Two : Added Gereric Class and Mathod

--- src/Generics.java
+++ src/Generics.java
@@ -1,17 +1,22 @@
-public class Generics {
- public String maxOfValues(String  str1, String str2,String str3) {
-        String max = str1;
-        if (str1.compareTo(max) > 0)
-            max = str2;
-        if (str3.compareTo(max) > 0)
-            max = str3;
+public class Generics <T extends Comparable<T>>{
+    T str1,str2,str3;
+    public Generics(T str1,T str2,T str3){
+        this.str1=str1;
+        this.str2=str2;
+        this.str3=str3;
+    }
+    public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
+        T max = a1;
+        if (a2.compareTo(max) > 0)
+            max = a2;
+        if (a3.compareTo(max) > 0)
+            max = a3;
         return max;
     }
-
     public static void main(String[] args) {
-        Generics generics=new Generics();
-        Generics gns=new Generics();
         System.out.println("\nWelcome To Find Maximum Value Problem Using Generics \n");
-        System.out.println("The maximum value between the three Number is : " +generics.maxOfValues("SSK","ABC","DSA"));
+        System.out.println("The maximum value between the three Number is : " +maxOfValues(1,9,3));
+        System.out.println("The maximum value between the three Number is : " +maxOfValues(2.35f,3.57f,7.37f));
+        System.out.println("The maximum value between the three Number is : " +maxOfValues("SSK","ABC","DSA"));
     }
 }
